b70c59de6e0acc5d045c6ea5d61393c892880274
ali <ali@ibashir.com>
2020-06-05 02:00:35 +0000

    add comments to data servlet that return in json format

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  ArrayList<String> comments = new ArrayList<String>(
+      Array.asList("Wow such website", "Yeah, good. Okay.", "Chicken."));
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<p>hello Ali!</p>");
+    
+    String commentsJson = new Gson().toJson(comments);
+    response.setContentType("application/json;");
+    response.getWriter().println(commentsJson);
+
   }
+
 }
